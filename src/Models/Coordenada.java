91543a676323ca7a07909c643f4e76495fcb90d5
Martin Dardis <martindardis96@gmail.com>
2018-11-11 12:09:16 -0300

    Agrego paquete Models

---
+++ src/Models/Coordenada.java
@@ -0,0 +1,5 @@
+package Models;
+
+public class Coordenada {
+    private int fila;
+}
---
+++ src/Models/Mapa.java
@@ -0,0 +1,34 @@
+
+package Models;
+
+import sun.jvm.hotspot.memory.Dictionary;
+
+import java.util.Map;
+
+class Mapa{
+
+    protected int filas;
+    protected int columnas;
+    protected Map campo;
+
+    public static void mapa(int ancho, int alto){
+        this.filas = alto;
+        this.columnas = ancho;
+        this.edificios = new Dictionary();
+    }
+
+    private
+
+    public void colocarEdificio(Edificio nuevo,int fila,int columna){
+
+
+    }
+
+    public void estaDisponible(int fila,int columna,int radio){
+
+    }
+
+
+
+
+}
