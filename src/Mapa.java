3a91e4c86c39536dd18710670b351e14f3797604
Martin Dardis <martindardis96@gmail.com>
2018-11-11 11:48:32 -0300

    Agrego configuracion de IDEA

---
+++ src/Mapa.java
@@ -0,0 +1,15 @@
+
+
+
+class Mapa{
+    protected int filas;
+    protected int columnas;
+
+
+    public void mapa(int ancho, int alto){
+        this.filas = alto;
+        this.columnas = ancho;
+    }
+
+
+}
