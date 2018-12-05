ac2f65a1b6c168f81233f8f5c84892da18d410f3
Gabriel Pucci <30637707+gabrielpucci@users.noreply.github.com>
2018-12-05 18:48:13 -0300

    Fix nombres

--- src/Controllers/BotonInicioHandler.java
+++ src/Controllers/BotonInicioHandler.java
@@ -6,6 +6,7 @@ import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -13,18 +14,25 @@ import Views.PantallaPrincipal;
 import Models.Partida.Partida;
 import Models.Posicionable;
 
+import javax.xml.soap.Text;
+
 public class BotonInicioHandler implements EventHandler<ActionEvent> {
 
     private Stage ventana;
     private Partida partida;
+    private TextField ingresoUno;
+    private TextField ingresoDos;
 
-    public BotonInicioHandler(Stage ventana, String jugadorUno, String jugadorDos){
+    public BotonInicioHandler(Stage ventana, TextField jugadorUno, TextField jugadorDos){
         this.ventana = ventana;
-        this.partida = new Partida(jugadorUno,jugadorDos);
+        this.ingresoUno = jugadorUno;
+        this.ingresoDos = jugadorDos;
+
     }
 
     @Override
     public void handle(ActionEvent e){
+        this.partida = new Partida(ingresoUno.getText(),ingresoDos.getText());
         PantallaPrincipal pantallaPrincipal = new PantallaPrincipal(this.ventana, this.partida);
         Scene escena = new Scene(pantallaPrincipal,1000,680);
         escena.getStylesheets().add("Views/estilo.css");
